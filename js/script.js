c039b4fb55bf2b12ce3ca096e88851d22e55c320
Psdannb <psdannb@gmail.com>
2026-01-07 09:30:34 +0545

    control structure

--- js/script.js
+++ js/script.js
@@ -6,3 +6,85 @@ alert("Welcome to javaScript"); */
 // firstName
 // first_name
 
+// data types  in js
+// let a = 5.45;
+// let a = "5";
+// let a = true;
+// let a = null;
+// console.log(typeof a);
+
+// control structure=> conditional statements, looping
+// conditional statement => if, if else, if else if, nested if else, switch
+// loop => for, while, do while loop
+
+// if
+/*if(condition){
+    block of code to execute if condition is true 
+}*/
+// let a = 10;
+// if (a > 0) {
+//   let msg = `${a} is positive number `;
+//   console.log(msg);
+// }
+
+/*
+if else syntax
+if(condition){
+    block of code to execute if condition is true 
+}
+else{
+     block of code to execute if condition is false
+} */
+
+// let a = -10;
+// if (a > 0) {
+//   let msg = `${a} is positive number `;
+//   console.log(msg);
+// } else {
+//   let msg = `${a} is negative number `;
+//   console.log(msg);
+// }
+
+/*
+if else syntax
+if(condition1){
+    block of code to execute if condition1 is true 
+}
+    elseif(condition2){
+      block of code to execute if condition2 is true 
+    }
+else{
+     block of code to execute if all the conditions are false
+} */
+
+// eg
+
+// let a = 0;
+// if (a > 0) {
+//   let msg = `${a} is positive number `;
+//   console.log(msg);
+// } else if (a < 0) {
+//   let msg = `${a} is negative number `;
+//   console.log(msg);
+// } else {
+//   let msg = ` The  number is ${a} `;
+//   console.log(msg);
+// }
+
+//h/w-
+//  write a js program to find the greatest number among three numbers.
+//  write a js program to find the number is even or odd.
+
+// nested if else
+// eg
+// let age = 18;
+// let hascitizenship = false;
+// if (age > 16) {
+//   if (hascitizenship) {
+//     console.log("You can have a voter id");
+//   } else {
+//     console.log("to make a voter id you need a citizenship");
+//   }
+// } else {
+//   console.log("Your age is not valid to have a citizenship card ");
+// }
