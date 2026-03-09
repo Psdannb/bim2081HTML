0bae2d5a8f8632f81f5148587e796ce04abf05c6
Psdannb <psdannb@gmail.com>
2026-03-09 09:44:02 +0545

    form validation and jquery added

---
+++ jquery/script.js
@@ -0,0 +1,32 @@
+// jquery selector
+
+$(document).ready(function () {
+  // id selector
+  //   let uservalue = $("#username").val();
+  //   console.log(uservalue);
+  //   class selector
+  //   let para = $(".paragraph");
+  //   console.log(para);
+  //   Element selector
+  //   let ptag = $("P");
+  //   console.log(ptag);
+  // input type text selector
+  //   let inputtypetext = $(":text");
+  //   console.log(inputtypetext);
+  //   event listener
+  //   let btn = $("#btn");
+  //   btn.on("click", function () {
+  //     alert("clicked");
+  //   });
+  //   $("#entryfrom").on("change", function () {
+  //     alert("change noticed");
+  //   });
+  //   $("#username").on("focus", function () {
+  //     // alert("focus noticed");
+  //     console.log("focus noticed");
+  //   });
+  // window/document events
+  //   $(document).on("scroll", function () {
+  //     alert("window scroll noticed");
+  //   });
+});
