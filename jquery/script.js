d6a1028e374f49d61bb710758d81393eb9c20445
Psdannb <psdannb@gmail.com>
2026-03-10 09:31:27 +0545

    jquery animation methods added

--- jquery/script.js
+++ jquery/script.js
@@ -29,4 +29,36 @@ $(document).ready(function () {
   //   $(document).on("scroll", function () {
   //     alert("window scroll noticed");
   //   });
+
+  $("#hidebtn").on("click", function () {
+    $("#textcontent").hide(2000);
+  });
+
+  $("#showbtn").on("click", function () {
+    $("#textcontent").show();
+  });
+  $("#animatebtn").on("click", function () {
+    $("#animatebtn").animate(
+      {
+        marginTop: "100px",
+        padding: "5px 10px",
+        borderRadius: "5px",
+      },
+      2000,
+      // complete(),
+    );
+  });
+  $("#stopbtn").on("click", function () {
+    $("#animatebtn").stop();
+  });
+
+  $("#fadebtn").on("click", function () {
+    $("#fadepara").fadeOut(5000);
+  });
+  $("#fadeinbtn").on("click", function () {
+    $("#fadepara").fadeIn(1000);
+  });
 });
+function complete() {
+  alert("animation is completed");
+}
